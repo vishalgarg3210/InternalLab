0ecc8e3bc0300c77d925bcb165f98a432c54fe8c
vishalgarg3210 <147580875+vishalgarg3210@users.noreply.github.com>
2023-11-18 14:34:49 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		 
+		System.out.println("welcome");
+		//System - predefined class 
+		//out --stream 
+		//println- method 
+
+	}
+
+}
