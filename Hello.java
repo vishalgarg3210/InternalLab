9639c06558bb9da3809d1f55c0dd3debeebfd104
Vishal <vishalgarg3210@gmail.com>
2023-11-18 14:11:58 +0530

    file Added

---
+++ Hello.java
@@ -0,0 +1,6 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java app ");  
+System.out.println("This is java app by using Jenkins");
+    }  
+    }  
