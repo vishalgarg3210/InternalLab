47a87b02c42cd3594f02b3cf4c20e62346c06560
vishalgarg3210 <147580875+vishalgarg3210@users.noreply.github.com>
2023-11-18 15:01:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{  
     public static void main(String[] args){  
     System.out.println("This is java app ");  
-System.out.println("This is java app by using Jenkins");
+System.out.println("This is java app by using Docker");
     }  
     }  
